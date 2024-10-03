84141312696aafb56169132003953e2c69344152
satyajit <satyajeetpatel96@gmail.com>
2024-10-03 20:30:55 +0530

    test2

--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -1,6 +1,6 @@
 """
 URL configuration for myapp project.
-
+#igigigi
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
